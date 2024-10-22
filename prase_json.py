1ef495c1de6806140c67099c53952bf02a408f28
WhereAreBugs <wherearebugs@icloud.com>
2024-10-22 15:58:00 +0800

    适配新平台

--- prase_json.py
+++ prase_json.py
@@ -6,39 +6,22 @@ with open('data.json', 'r') as f:
 # 读取json数据
 data = json.loads(input)
 # 读取json数据中的data
-data = data['data']
+data = data['PageList']['Items']
 # 写入CSV文件的表头
 with open('data.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['活动名称', '活动状态', '学期', '活动积分', '结束时间', '开始时间', '活动ID'])
+    writer.writerow(['活动名称', '活动状态', '签到状态', '时间', '活动ID'])
 
 # 循环读取data中的每一条数据,并输出到CSV文件中
 for item in data:
     if item is None:
         continue
-    name = item['activityName']
-    activityStatus = item['activityStatus']
-    if activityStatus == '0':
-        activityStatus = '报名中'
-    elif activityStatus == '1':
-        activityStatus = '待开始'
-    elif activityStatus == '2':
-        activityStatus = '进行中'
-    elif activityStatus == '3':
-        activityStatus = '待完结'
-    elif activityStatus == '4':
-        activityStatus = '完结审核中'
-    elif activityStatus == '5':
-        activityStatus = '已完结'
-    else:
-        activityStatus = '未知'
-
-    activityId = item['id']
-    semester = item['semester']
-    activityIntegral = item['activityIntegral']
-    endTime = item['endTime']
-    startTime = item['startTime']
+    name = item['ActivityName']
+    activityId = item['ActivityId']
+    Examstatus = item['ExamStatus']
+    SignD = item['SignD']
+    time = item['ActivityTime']
     with open('data.csv', 'a', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow([name, activityStatus, semester, activityIntegral, endTime, startTime, activityId])
+        writer.writerow([name, Examstatus, SignD, time, activityId])
 
--- signCode.py
+++ signCode.py
@@ -2,24 +2,18 @@ import requests
 import qrcode
 import time
 
-# 登陆地址: http://ekty.cuit.edu.cn/#/pages/home/login
-# 登录API: http://ekty.cuit.edu.cn/api/login?sf_request_type=ajax
-
-# playload: 请求体
-# {
-#   "account": "",
-#   "password": ""
-# }
-
 # 当前暂未实现自动化获取二维码,先完成一个手动的版本
 
-prefix = "http://ekty-cuit-edu-cn.webvpn.cuit.edu.cn:8118/#/pages/activity/studentQdqt?id="
 id = input("请输入活动ID: ")
-now_time = int(round(time.time() * 1000))
-now_time += 7200000  # 获取两小时之后的时间
-url = prefix + id + "&timestamp=" + str(now_time)
-print("签到链接: " + url)
-print("有效期" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now_time/1000)))
-img = qrcode.make(url)
-print("二维码已生成,请在当前目录下查看(qrcode.png)")
-img.save("qrcode.png")
+now_time = int(round(time.time()))
+url_QD = id + "|"+str(now_time) +"|"+ "QD"
+print("有效期" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now_time)))
+img = qrcode.make(url_QD)
+print("签到二维码已生成,请在当前目录下查看(qrcode_QD.png)")
+img.save("qrcode_QD.png")
+
+url_QT = id +"|"+ str(now_time) +"|"+ "QT"
+print("有效期" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now_time)))
+img = qrcode.make(url_QT)
+print("签退二维码已生成,请在当前目录下查看(qrcode_QT.png)")
+img.save("qrcode_QT.png")
